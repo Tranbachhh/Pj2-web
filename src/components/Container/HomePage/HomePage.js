5deb25d72c19502ae71a49791a93ad2ef03cc0c4
unknown <tranbachchy2002@gmail.com>
2023-06-01 12:40:20 +0700

    'update'

--- src/components/Container/HomePage/HomePage.js
+++ src/components/Container/HomePage/HomePage.js
@@ -1,207 +1,331 @@
 import Toolbar from "../../Layout/Toolbar/Toolbar"
-import ShowListItem from '../../Layout/ShowListItem/ShowListItem'
 import ListImg from "../../Layout/ListImg/ListImg"
-import Menu from "../../Layout/Menu/Menu"
 import Footer from "../../Layout/Footer/Footer"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Item from "../../Layout/Item/Item"
 
-import styles from './HomePage.module.css'
-import { faBoltLightning } from "@fortawesome/free-solid-svg-icons"
+function HomePage() {
 
-const items=[
-  {
-    _id: "123456",
-    image: [
-      "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
-      "product-1681974909771-iphone-13-starlight.jpg",
-      "product-1681974909771-iPhone-14.jpg",
-    ],
-    name: "Iphone 14 Pro Max 128Gb",
-    description: "Sản phẩm mới nhất của nhãn hàng Iphone",
-    detail: {
-      rear_camera: "14",
-      front_camera: "14",
-      operating_system: "IOS",
-      display_size: "6.1",
-      power: "3279",
-      memory: "128",
-      ram: "4",
-    },
-    category: "Iphone",
-    price: 29999990,
-    discount: 5,
-  },{
-    _id: "123456",
-    image: [
-      "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
-      "product-1681974909771-iphone-13-starlight.jpg",
-      "product-1681974909771-iPhone-14.jpg",
-    ],
-    name: "Iphone 14 Pro Max 128Gb",
-    description: "Sản phẩm mới nhất của nhãn hàng Iphone",
-    detail: {
-      rear_camera: "14",
-      front_camera: "14",
-      operating_system: "IOS",
-      display_size: "6.1",
-      power: "3279",
-      memory: "128",
-      ram: "4",
-    },
-    category: "Iphone",
-    price: 29999990,
-    discount: 5,
-  },{
-    _id: "123456",
-    image: [
-      "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
-      "product-1681974909771-iphone-13-starlight.jpg",
-      "product-1681974909771-iPhone-14.jpg",
-    ],
-    name: "Iphone 14 Pro Max 128Gb",
-    description: "Sản phẩm mới nhất của nhãn hàng Iphone",
-    detail: {
-      rear_camera: "14",
-      front_camera: "14",
-      operating_system: "IOS",
-      display_size: "6.1",
-      power: "3279",
-      memory: "128",
-      ram: "4",
-    },
-    category: "Iphone",
-    price: 29999990,
-    discount: 5,
-  },{
-    _id: "123456",
-    image: [
-      "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
-      "product-1681974909771-iphone-13-starlight.jpg",
-      "product-1681974909771-iPhone-14.jpg",
-    ],
-    name: "Iphone 14 Pro Max 128Gb",
-    description: "Sản phẩm mới nhất của nhãn hàng Iphone",
-    detail: {
-      rear_camera: "14",
-      front_camera: "14",
-      operating_system: "IOS",
-      display_size: "6.1",
-      power: "3279",
-      memory: "128",
-      ram: "4",
-    },
-    category: "Iphone",
-    price: 29999990,
-    discount: 5,
-  },{
-    _id: "123456",
-    image: [
-      "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
-      "product-1681974909771-iphone-13-starlight.jpg",
-      "product-1681974909771-iPhone-14.jpg",
-    ],
-    name: "Iphone 14 Pro Max 128Gb",
-    description: "Sản phẩm mới nhất của nhãn hàng Iphone",
-    detail: {
-      rear_camera: "14",
-      front_camera: "14",
-      operating_system: "IOS",
-      display_size: "6.1",
-      power: "3279",
-      memory: "128",
-      ram: "4",
-    },
-    category: "Iphone",
-    price: 29999990,
-    discount: 5,
-  },{
-    _id: "123456",
-    image: [
-      "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
-      "product-1681974909771-iphone-13-starlight.jpg",
-      "product-1681974909771-iPhone-14.jpg",
-    ],
-    name: "Iphone 14 Pro Max 128Gb",
-    description: "Sản phẩm mới nhất của nhãn hàng Iphone",
-    detail: {
-      rear_camera: "14",
-      front_camera: "14",
-      operating_system: "IOS",
-      display_size: "6.1",
-      power: "3279",
-      memory: "128",
-      ram: "4",
-    },
-    category: "Iphone",
-    price: 29999990,
-    discount: 5,
-  },{
-    _id: "123456",
-    image: [
-      "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
-      "product-1681974909771-iphone-13-starlight.jpg",
-      "product-1681974909771-iPhone-14.jpg",
-    ],
-    name: "Iphone 14 Pro Max 128Gb",
-    description: "Sản phẩm mới nhất của nhãn hàng Iphone",
-    detail: {
-      rear_camera: "14",
-      front_camera: "14",
-      operating_system: "IOS",
-      display_size: "6.1",
-      power: "3279",
-      memory: "128",
-      ram: "4",
-    },
-    category: "Iphone",
-    price: 29999990,
-    discount: 5,
-  },{
-    _id: "123456",
-    image: [
-      "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
-      "product-1681974909771-iphone-13-starlight.jpg",
-      "product-1681974909771-iPhone-14.jpg",
-    ],
-    name: "Iphone 14 Pro Max 128Gb",
-    description: "Sản phẩm mới nhất của nhãn hàng Iphone",
-    detail: {
-      rear_camera: "14",
-      front_camera: "14",
-      operating_system: "IOS",
-      display_size: "6.1",
-      power: "3279",
-      memory: "128",
-      ram: "4",
-    },
-    category: "Iphone",
-    price: 29999990,
-    discount: 5,
-  }
-]
-
-
-function HomePage(){
-
-    return(
-        <div className={styles.homePage}>
-            <Toolbar />
-            <ListImg />
-            <Menu />
-            <div>
-              <img alt='khuyenmai' src={require('./km.png')} width={1440} />
-            </div>
-            <div className={styles.khuyenmai}>
-              <span className={styles.kmhot}><FontAwesomeIcon icon={faBoltLightning}/> KHUYẾN MÃI HOT</span>
-              <ShowListItem items={items}/>
-            </div>
-            <div className={styles.iphone}> 
-              <span className={styles.ipname}>ĐIỆN THOẠI IPHONE</span>
-              <ShowListItem items={items}/>
-            </div>
-            <Footer />
+  const hotProducts = [
+    {
+      _id: "123456",
+      image: [
+        "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
+        "product-1681974909771-iphone-13-starlight.jpg",
+        "product-1681974909771-iPhone-14.jpg",
+      ],
+      name: "Iphone 14 Pro Max 128Gb",
+      description: "Sản phẩm mới nhất của nhãn hàng Iphone",
+      detail: {
+        rear_camera: "14",
+        front_camera: "14",
+        operating_system: "IOS",
+        display_size: "6.1",
+        power: "3279",
+        memory: "128",
+        ram: "4",
+      },
+      category: "Iphone",
+      price: 29999990,
+      discount: 5,
+    },{
+      _id: "123456",
+      image: [
+        "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
+        "product-1681974909771-iphone-13-starlight.jpg",
+        "product-1681974909771-iPhone-14.jpg",
+      ],
+      name: "Iphone 14 Pro Max 128Gb",
+      description: "Sản phẩm mới nhất của nhãn hàng Iphone",
+      detail: {
+        rear_camera: "14",
+        front_camera: "14",
+        operating_system: "IOS",
+        display_size: "6.1",
+        power: "3279",
+        memory: "128",
+        ram: "4",
+      },
+      category: "Iphone",
+      price: 29999990,
+      discount: 5,
+    },{
+      _id: "123456",
+      image: [
+        "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
+        "product-1681974909771-iphone-13-starlight.jpg",
+        "product-1681974909771-iPhone-14.jpg",
+      ],
+      name: "Iphone 14 Pro Max 128Gb",
+      description: "Sản phẩm mới nhất của nhãn hàng Iphone",
+      detail: {
+        rear_camera: "14",
+        front_camera: "14",
+        operating_system: "IOS",
+        display_size: "6.1",
+        power: "3279",
+        memory: "128",
+        ram: "4",
+      },
+      category: "Iphone",
+      price: 29999990,
+      discount: 5,
+    },{
+      _id: "123456",
+      image: [
+        "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
+        "product-1681974909771-iphone-13-starlight.jpg",
+        "product-1681974909771-iPhone-14.jpg",
+      ],
+      name: "Iphone 14 Pro Max 128Gb",
+      description: "Sản phẩm mới nhất của nhãn hàng Iphone",
+      detail: {
+        rear_camera: "14",
+        front_camera: "14",
+        operating_system: "IOS",
+        display_size: "6.1",
+        power: "3279",
+        memory: "128",
+        ram: "4",
+      },
+      category: "Iphone",
+      price: 29999990,
+      discount: 5,
+    }]
+  
+    const iphoneProducts = [
+      {
+        _id: "123456",
+        image: [
+          "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
+          "product-1681974909771-iphone-13-starlight.jpg",
+          "product-1681974909771-iPhone-14.jpg",
+        ],
+        name: "Iphone 14 Pro Max 128Gb",
+        description: "Sản phẩm mới nhất của nhãn hàng Iphone",
+        detail: {
+          rear_camera: "14",
+          front_camera: "14",
+          operating_system: "IOS",
+          display_size: "6.1",
+          power: "3279",
+          memory: "128",
+          ram: "4",
+        },
+        category: "Iphone",
+        price: 29999990,
+        discount: 5,
+      },{
+        _id: "123456",
+        image: [
+          "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
+          "product-1681974909771-iphone-13-starlight.jpg",
+          "product-1681974909771-iPhone-14.jpg",
+        ],
+        name: "Iphone 14 Pro Max 128Gb",
+        description: "Sản phẩm mới nhất của nhãn hàng Iphone",
+        detail: {
+          rear_camera: "14",
+          front_camera: "14",
+          operating_system: "IOS",
+          display_size: "6.1",
+          power: "3279",
+          memory: "128",
+          ram: "4",
+        },
+        category: "Iphone",
+        price: 29999990,
+        discount: 5,
+      },{
+        _id: "123456",
+        image: [
+          "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
+          "product-1681974909771-iphone-13-starlight.jpg",
+          "product-1681974909771-iPhone-14.jpg",
+        ],
+        name: "Iphone 14 Pro Max 128Gb",
+        description: "Sản phẩm mới nhất của nhãn hàng Iphone",
+        detail: {
+          rear_camera: "14",
+          front_camera: "14",
+          operating_system: "IOS",
+          display_size: "6.1",
+          power: "3279",
+          memory: "128",
+          ram: "4",
+        },
+        category: "Iphone",
+        price: 29999990,
+        discount: 5,
+      },{
+        _id: "123456",
+        image: [
+          "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
+          "product-1681974909771-iphone-13-starlight.jpg",
+          "product-1681974909771-iPhone-14.jpg",
+        ],
+        name: "Iphone 14 Pro Max 128Gb",
+        description: "Sản phẩm mới nhất của nhãn hàng Iphone",
+        detail: {
+          rear_camera: "14",
+          front_camera: "14",
+          operating_system: "IOS",
+          display_size: "6.1",
+          power: "3279",
+          memory: "128",
+          ram: "4",
+        },
+        category: "Iphone",
+        price: 29999990,
+        discount: 5,
+      }]
+  
+      const samsungProducts = [
+        {
+          _id: "123456",
+          image: [
+            "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
+            "product-1681974909771-iphone-13-starlight.jpg",
+            "product-1681974909771-iPhone-14.jpg",
+          ],
+          name: "Iphone 14 Pro Max 128Gb",
+          description: "Sản phẩm mới nhất của nhãn hàng Iphone",
+          detail: {
+            rear_camera: "14",
+            front_camera: "14",
+            operating_system: "IOS",
+            display_size: "6.1",
+            power: "3279",
+            memory: "128",
+            ram: "4",
+          },
+          category: "Iphone",
+          price: 29999990,
+          discount: 5,
+        },{
+          _id: "123456",
+          image: [
+            "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
+            "product-1681974909771-iphone-13-starlight.jpg",
+            "product-1681974909771-iPhone-14.jpg",
+          ],
+          name: "Iphone 14 Pro Max 128Gb",
+          description: "Sản phẩm mới nhất của nhãn hàng Iphone",
+          detail: {
+            rear_camera: "14",
+            front_camera: "14",
+            operating_system: "IOS",
+            display_size: "6.1",
+            power: "3279",
+            memory: "128",
+            ram: "4",
+          },
+          category: "Iphone",
+          price: 29999990,
+          discount: 5,
+        },{
+          _id: "123456",
+          image: [
+            "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
+            "product-1681974909771-iphone-13-starlight.jpg",
+            "product-1681974909771-iPhone-14.jpg",
+          ],
+          name: "Iphone 14 Pro Max 128Gb",
+          description: "Sản phẩm mới nhất của nhãn hàng Iphone",
+          detail: {
+            rear_camera: "14",
+            front_camera: "14",
+            operating_system: "IOS",
+            display_size: "6.1",
+            power: "3279",
+            memory: "128",
+            ram: "4",
+          },
+          category: "Iphone",
+          price: 29999990,
+          discount: 5,
+        },{
+          _id: "123456",
+          image: [
+            "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
+            "product-1681974909771-iphone-13-starlight.jpg",
+            "product-1681974909771-iPhone-14.jpg",
+          ],
+          name: "Iphone 14 Pro Max 128Gb",
+          description: "Sản phẩm mới nhất của nhãn hàng Iphone",
+          detail: {
+            rear_camera: "14",
+            front_camera: "14",
+            operating_system: "IOS",
+            display_size: "6.1",
+            power: "3279",
+            memory: "128",
+            ram: "4",
+          },
+          category: "Iphone",
+          price: 29999990,
+          discount: 5,
+        }]
+  return (
+      <div className="container g-0 homepage-wrapper">
+        <Toolbar />
+        <ListImg />
+        <div className="row mt-3 container-fluid block-content">
+          <div className="block-content-title">
+            <h2>Khuyến mãi hot</h2>
+            <span>Xem tất cả</span>
+          </div>
+          {hotProducts.map((product, index) => {
+            return (
+              <div
+                key={index}
+                className="col-lg-3 col-md-4 col-sm-6 col-12 no-gutter"
+              >
+                <Item product={product} />
+              </div>
+            );
+          })}
         </div>
-    )
+
+        {iphoneProducts.length > 0 && (
+          <div className="row mt-3 container-fluid block-content">
+            <div className="block-content-title">
+              <h2>Điện thoại Iphone</h2>
+              <span>Xem tất cả</span>
+            </div>
+            {iphoneProducts.map((product, index) => {
+              return (
+                <div
+                  key={index}
+                  className="col-lg-3 col-md-4 col-sm-6 col-12 no-gutter"
+                >
+                  <Item product={product} />
+                </div>
+              );
+            })}
+          </div>
+        )}
+
+        {samsungProducts.length > 0 && (
+          <div className="row mt-3 container-fluid block-content">
+            <div className="block-content-title">
+              <h2>Điện thoại Samsung</h2>
+              <span>Xem tất cả</span>
+            </div>
+            {samsungProducts.map((product, index) => {
+              return (
+                <div
+                  key={index}
+                  className="col-lg-3 col-md-4 col-sm-6 col-12 no-gutter"
+                >
+                  <Item product={product} />
+                </div>
+              );
+            })}
+          </div>
+        )}
+      <Footer />
+      </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
--- src/components/Layout/Item/Item.js
+++ src/components/Layout/Item/Item.js
@@ -1,72 +1,113 @@
-import { faAppleAlt, faCameraAlt, faMobileScreen, faSdCard } from '@fortawesome/free-solid-svg-icons';
-import styles from './Item.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faArrowDown,
+  faMicrochip,
+  faMobileScreen,
+  faSdCard,
+} from "@fortawesome/free-solid-svg-icons";
+import "./Card.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Item( {item = {
-  _id: "123456",
-  image: [
-    "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
-    "product-1681974909771-iphone-13-starlight.jpg",
-    "product-1681974909771-iPhone-14.jpg",
-  ],
-  name: "Iphone 14 Pro Max 128Gb",
-  description: "Sản phẩm mới nhất của nhãn hàng Iphone",
-  detail: {
-    rear_camera: "14",
-    front_camera: "14",
-    operating_system: "IOS",
-    display_size: "6.1",
-    power: "3279",
-    memory: "128",
-    ram: "4",
+function Item({
+  product = 
+  {
+    _id: "123456",
+    image: [
+      "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/1896224739.jpeg",
+      "product-1681974909771-iphone-13-starlight.jpg",
+      "product-1681974909771-iPhone-14.jpg",
+    ],
+    name: "Iphone 14 Pro Max 128Gb",
+    description: "Sản phẩm mới nhất của nhãn hàng Iphone",
+    detail: {
+      rear_camera: "14",
+      front_camera: "14",
+      operating_system: "IOS",
+      display_size: "6.1",
+      power: "3279",
+      memory: "128",
+      ram: "4",
+    },
+    category: "Iphone",
+    price: 29999990,
+    discount: 5,
   },
-  category: "Iphone",
-  price: 29999990,
-  discount: 5,
-},className=""}){
+  className = "",
+}) {
+  //const classes = ["product", className].join(" ");
 
-    const classes = [
-        styles.item,
-        className,
-      ].join(" ");
+  // convert number of price to VND format
+  const convertPriceToString = (price) => {
+    return price.toLocaleString("it-IT", {
+      style: "currency",
+      currency: "VND",
+    });
+  };
 
-    const convertPriceToString = (price) => {
-        return price.toLocaleString("it-IT", {
-          style: "currency",
-          currency: "VND",
-        });
-      };
 
-    return(
-        
-        <div className={classes}>
-            <div className={styles.price_discount}> 
-                <div className={[styles.price,"col-md-6"].join(" ")}>
-                    Giảm {convertPriceToString(Math.round(item.price*item.discount/100))}
-                </div>
-                <div className={[styles.discount,"col-md-4"].join(" ")}>Trả góp  0%</div>
-            </div>
-            <img src={item.image[0]} alt={item.name} />
-            <div className={styles.name}>
-                {item.name}
-            </div>
-            <div className={styles.price_1}>{convertPriceToString(item.price)}</div>
-            <div className={styles.price_discount}> 
-                <div className={[styles.price,"col-md-6"].join(" ")}>
-                    {convertPriceToString(item.price-Math.round(item.price*item.discount/100))}
-                </div>
-                <div className={[styles.discount,"col-md-4"].join(" ")}>Giảm : {item.discount}%</div>
-            </div>
-            <hr />
-            <div className={styles.Info}>
-                <span className={styles.infoItem}><FontAwesomeIcon icon={faAppleAlt} /> Hệ điều hành : {item.detail.operating_system}</span> 
-                <span className={styles.infoItem}><FontAwesomeIcon icon={faSdCard} /> RAM: {item.detail.ram} G</span>
-                <span className={styles.infoItem}><FontAwesomeIcon icon={faMobileScreen} /> Màn hình : {item.detail.display_size} Inch</span>
-                <span className={styles.infoItem}><FontAwesomeIcon icon={faCameraAlt} /> Camera: {item.detail.rear_camera} MP</span>
-            </div>
+  const handleClickCard = ()=>{
+
+  }
+
+  return product !== null ? (
+    <div className="card-container container-fluid" onClick={handleClickCard}>
+      <div className="row flex-column">
+        <span className="col-12">Giảm {convertPriceToString(3000000)}</span>
+        <span className="col-12">Trả góp 0%</span>
+      </div>
+      <div
+        className="product-image container-fluid"
+        style={{
+          backgroundImage: `url(${product.image[0]})`,
+        }}
+      ></div>
+      <div className="product-info container-fluid">
+        <div className="row">
+          <h5
+            style={{
+              padding: 0,
+              textTransform: "capitalize",
+            }}
+          >
+            {product.name}
+          </h5>
         </div>
-    )
+        <div className="row">
+          <div className="col-12 col-lg-6 px-2 price">
+            {product.discount > 0
+              ? convertPriceToString(
+                  (product.price * (100 - product.discount)) / 100
+                )
+              : convertPriceToString(product.price)}
+          </div>
 
+          {product.discount > 0 && (
+            <div className="col-12 col-lg-6 px-2 discount">
+              <FontAwesomeIcon icon={faArrowDown} />
+              {product.discount}%
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="product-detail container-fluid row g-0">
+        <div className="col-6 col-sm-12 col-md-12 col-lg-6">
+          <FontAwesomeIcon icon={faMicrochip} />{" "}
+          {product.detail.operating_system}
+        </div>
+        <div className="col-6 col-sm-12 col-md-12 col-lg-6">
+          <FontAwesomeIcon icon={faMobileScreen} />{" "}
+          {product.detail.display_size} Inch
+        </div>
+        <div className="col-6 col-sm-12 col-md-12 col-lg-6">
+          <FontAwesomeIcon icon={faSdCard} /> {product.detail.ram} Gb
+        </div>
+        <div className="col-6 col-sm-12 col-md-12 col-lg-6">
+          <FontAwesomeIcon icon={faSdCard} /> {product.detail.memory} Gb
+        </div>
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
 }
 
-export default Item
+export default Item;
